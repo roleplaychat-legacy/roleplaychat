e1161be7aa516db1b931d8df3784dfde10c51b16
xunto <hunterdelyx1@gmail.com>
2018-11-09 18:01:30 +0300

    Fix to-gm doesn't work between dimensions

--- src/main/java/ru/xunto/roleplaychat/features/ToGmMiddleware.java
+++ src/main/java/ru/xunto/roleplaychat/features/ToGmMiddleware.java
@@ -1,19 +1,26 @@
 package ru.xunto.roleplaychat.features;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 import net.minecraftforge.server.permission.PermissionAPI;
 import ru.xunto.roleplaychat.framework.api.Environment;
 import ru.xunto.roleplaychat.framework.api.Middleware;
 import ru.xunto.roleplaychat.framework.api.Request;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class ToGmMiddleware extends Middleware {
     @Override public void process(Request request, Environment environment) {
         Set<EntityPlayer> recipients = environment.getRecipients();
-        for (EntityPlayer player : request.getWorld().playerEntities) {
-            if (PermissionAPI.hasPermission(player, "gm"))
-                recipients.add(player);
+
+        World[] worlds = Objects.requireNonNull(request.getWorld().getMinecraftServer()).worlds;
+
+        for (World world : worlds) {
+            for (EntityPlayer player : world.playerEntities) {
+                if (PermissionAPI.hasPermission(player, "gm"))
+                    recipients.add(player);
+            }
         }
     }
 }
