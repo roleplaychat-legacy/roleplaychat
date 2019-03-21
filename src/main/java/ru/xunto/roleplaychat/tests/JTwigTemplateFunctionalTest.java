a2fd59d143822eb328b321384489c8037b6c730e
xunto <hunterdelyx1@gmail.com>
2019-03-21 23:13:05 +0300

    Tests

---
+++ src/main/java/ru/xunto/roleplaychat/tests/JTwigTemplateFunctionalTest.java
@@ -0,0 +1,83 @@
+package ru.xunto.roleplaychat.tests;
+
+import net.minecraft.util.text.TextFormatting;
+import org.junit.Test;
+import ru.xunto.roleplaychat.framework.api.Environment;
+import ru.xunto.roleplaychat.framework.jtwig.JTwigState;
+import ru.xunto.roleplaychat.framework.jtwig.JTwigTemplate;
+import ru.xunto.roleplaychat.framework.renderer.ITemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static ru.xunto.roleplaychat.features.endpoints.ActionEndpoint.ACTION_PARTS;
+import static ru.xunto.roleplaychat.features.endpoints.ActionEndpoint.START_WITH_ACTION;
+
+public class JTwigTemplateFunctionalTest {
+
+    public static void testCase(ITemplate template, JTwigState state, String expectedResult) {
+        Map<String, TextFormatting> colors = new HashMap<>();
+        String result = template.render(state, colors).getUnformattedText();
+        assertEquals(result, expectedResult);
+    }
+
+
+    public static JTwigState setUpState() {
+        JTwigState state = new JTwigState();
+        state.setValue(Environment.USERNAME, "username");
+        state.setValue(Environment.TEXT, "text");
+        return state;
+    }
+
+    @Test public void testRenderDefault() {
+        ITemplate template;
+        JTwigState state;
+
+        state = setUpState();
+        template = new JTwigTemplate("templates/default.twig");
+        testCase(template, state, "username: text");
+        state.setValue(Environment.LABEL, "label");
+
+        testCase(template, state, "username (label): text");
+
+    }
+
+    @Test public void testRenderGmOOC() {
+        JTwigState state = setUpState();
+        ITemplate template = new JTwigTemplate("templates/gm_ooc.twig");
+        testCase(template, state, "username (To GM): (( text ))");
+    }
+
+    @Test public void testRenderOOC() {
+        ITemplate template;
+        JTwigState state;
+
+        state = setUpState();
+        template = new JTwigTemplate("templates/ooc.twig");
+        testCase(template, state, "username (OOC): (( text ))");
+        state.setValue(Environment.LABEL, "label");
+        testCase(template, state, "username (label): (( text ))");
+    }
+
+    @Test public void testRenderAction() {
+        ITemplate template;
+        JTwigState state;
+
+        state = setUpState();
+        template = new JTwigTemplate("templates/action.twig");
+
+        state.setValue(ACTION_PARTS, new String[] {"text1", "text2", "text3", "text4"});
+
+        state.setValue(START_WITH_ACTION, false);
+        testCase(template, state, "username: text1 * text2 * text3 * text4 *");
+        state.setValue(START_WITH_ACTION, true);
+        testCase(template, state, "* username text1 * text2 * text3 * text4");
+
+        state.setValue(Environment.LABEL, "label");
+        state.setValue(START_WITH_ACTION, false);
+        testCase(template, state, "username (label): text1 * text2 * text3 * text4 *");
+        state.setValue(START_WITH_ACTION, true);
+        testCase(template, state, "* username (label) text1 * text2 * text3 * text4");
+    }
+}
