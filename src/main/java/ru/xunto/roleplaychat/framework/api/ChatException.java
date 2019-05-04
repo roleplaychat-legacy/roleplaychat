89a324b0def71114b8131a897467d98a1133d92a
xunto <hunterdelyx1@gmail.com>
2019-05-04 19:00:09 +0300

    constructors

--- src/main/java/ru/xunto/roleplaychat/framework/api/ChatException.java
+++ src/main/java/ru/xunto/roleplaychat/framework/api/ChatException.java
@@ -1,4 +1,11 @@
 package ru.xunto.roleplaychat.framework.api;
 
 public class ChatException extends Exception {
+    public ChatException() {
+        super();
+    }
+
+    public ChatException(String message) {
+        super(message);
+    }
 }
